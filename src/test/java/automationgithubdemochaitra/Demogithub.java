06611acc9ed159fa5ea1c508da3be04d1030b3e7
NIRNAY <NIRNAY@DESKTOP-AT5VQ62.lan>
2024-04-01 15:35:07 +0530

    demo project

---
+++ src/test/java/automationgithubdemochaitra/Demogithub.java
@@ -0,0 +1,17 @@
+package automationgithubdemochaitra;
+
+public class Demogithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+	}	
+
+}
+
+
+// create local repository 
+/** update project and right click, Team, shareproject
+ * check the two  check box  and finish*/
+ */
+ // commit the pro
